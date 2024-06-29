602744e7a36eafb949f591a4cb44d129456e11af
Nuelo <akalo.emmanuel18@gmail.com>
2024-06-29 18:59:51 +0100

    refact: code implementation'

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,16 @@
 # This puppet manifest installs flask from pip3.
 
+package { 'python3':
+    ensure => present,
+}
+
+package { 'python3-pip':
+    ensure  => present,
+    require => Package['python3'],
+}
+
 package {'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
+    require  => Package['python3-pip'],
 }
