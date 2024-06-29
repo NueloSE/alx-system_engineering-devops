d2dd498aaceba5827570f6f3b2b5a1de87f109e9
Nuelo <akalo.emmanuel18@gmail.com>
2024-06-29 18:48:39 +0100

    chore: format style

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,6 @@
 # This puppet manifest installs flask from pip3.
 
-package {
-    'flask':
+package {'flask':
     ensure   => '2.1.0',
-    name     => 'flask',
-    provider => 'pip3'
+    provider => 'pip3',
 }
