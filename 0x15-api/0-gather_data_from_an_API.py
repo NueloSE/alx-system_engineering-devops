70d455a3f8e30a045744498b79449135ba1935ed
Nuelo <akalo.emmanuel18@gmail.com>
2024-08-10 08:31:52 +0100

    chore: fix pep8 issues

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,20 +13,23 @@ def todo_list():
     url = "https://jsonplaceholder.typicode.com/"
     users = (requests.get(url + "users/{}".format(employee_id))).json()
     todos = (requests.get(url + "todos/")).json()
-    employee_name = users["name"]
-    completed_task = 0
-    assigned_task = 0
-    task_title = []
+    name = users["name"]
+    taskDone = 0
+    taskCount = 0
+    taskTitle = []
 
     for todo in todos:
         if employee_id == todo['userId']:
-            assigned_task += 1
+            taskCount += 1
             if todo["completed"]:
-                completed_task += 1
-                task_title.append(todo['title'])
-    output = "Employee {} is done with task({}/{}):"
-    print(output.format(employee_name, completed_task, assigned_task))
-    for title in task_title:
+                taskDone += 1
+                taskTitle.append(todo['title'])
+    output = (
+        "Employee {} is done with tasks({}/{}):"
+        .format(name, taskDone, taskCount)
+    )
+    print(output)
+    for title in taskTitle:
         print(f"\t {title}")
 
 
