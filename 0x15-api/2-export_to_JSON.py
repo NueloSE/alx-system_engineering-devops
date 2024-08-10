42a5328c3650416c8894211519103a4d2f001431
Nuelo <akalo.emmanuel18@gmail.com>
2024-08-10 14:07:40 +0100

    feat: implement API to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Implement API call to CSV"""
+import requests
+from sys import argv
+import json
+
+
+def api_to_json():
+    user_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    users = (requests.get(url + "users/" + user_id)).json()
+    todos = (requests.get(url + "todos")).json()
+    userName = users["username"]
+    userDetails = []
+
+    for todo in todos:
+        if todo["userId"] == int(user_id):
+            taskDict = {
+                "task": todo["title"], "completed": todo["completed"],
+                "username": userName
+            }
+            userDetails.append(taskDict)
+    taskDict = {user_id: userDetails}
+
+    with open(f"{user_id}.json", 'w') as file:
+        json.dump(taskDict, file)
+
+
+if __name__ == "__main__":
+    api_to_json()
