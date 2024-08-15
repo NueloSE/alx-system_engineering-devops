31cdaefc312a8ce867307f53e785e00ddd94cf7a
Nuelo <akalo.emmanuel18@gmail.com>
2024-08-15 11:12:26 +0100

    fix: pycodestyle

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,15 +3,16 @@
 
 import requests
 
+
 def top_ten(subreddit):
-	"""gets top ten items in subreddit api"""
-	if subreddit is None or not isinstance(subreddit, str):
-		print("None")
-	try:
-		url = f"http://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-		response = requests.get(url)
-		result = response.json()["data"]["children"]
-		for item in result:
-			print(item["data"]['title'])
-	except Exception:
-		print("None")
+    """gets top ten items in subreddit api"""
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+    try:
+        url = f"http://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+        response = requests.get(url)
+        result = response.json()["data"]["children"]
+        for item in result:
+            print(item["data"]['title'])
+    except Exception:
+        print("None")
